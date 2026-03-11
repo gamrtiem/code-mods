10531518b470bd84a0df4cbad7de2df0a91f207e
icebro <potatotot98@gmail.com>
2026-03-11 05:32:21 -0600

    testscene skybox recolor .,.,

--- butterscotchnroses/butterscotchnroses/butterscotchnroses.cs
+++ butterscotchnroses/butterscotchnroses/butterscotchnroses.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using AK.Wwise;
@@ -12,6 +13,8 @@ using UnityEngine;
 using UnityEngine.AddressableAssets;
 using SceneDirector = On.RoR2.SceneDirector;
 using ShaderSwapper;
+using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
 
 namespace BNR
 {
@@ -68,6 +71,25 @@ namespace BNR
                 item.Init(Config);
             }
             
+            On.RoR2.SceneDirector.Start += SceneDirectorOnStart;
+        }
+
+        public static Material skyboxMaterial;
+        private void SceneDirectorOnStart(SceneDirector.orig_Start orig, RoR2.SceneDirector self)
+        {
+            orig(self);
+            if (SceneManager.GetActiveScene().name == "testscene")
+            {
+                Log.Debug($"new scene !! {RenderSettings.skybox} {SceneManager.GetActiveScene().name}");
+                
+                if (skyboxMaterial != null)
+                {
+                    skyboxMaterial = Object.Instantiate(RenderSettings.skybox);
+                    skyboxMaterial.SetColor("_Tint", BNRUtils.Color255(152, 122, 144, 255));
+                }
+                
+                RenderSettings.skybox = skyboxMaterial;
+            }
             
         }
 
@@ -79,6 +101,58 @@ namespace BNR
                 UnityHotReloadNS.UnityHotReload.LoadNewAssemblyVersion(typeof(butterscotchnroses).Assembly, System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Info.Location)!, "butterscotchnroses.dll"));
             }
 #endif  
+        }
+        
+        private static List<uint> sounds = [];
+        [ConCommand(commandName = "play_sound", flags = ConVarFlags.None, helpText = "https://risk-of-thunder.github.io/R2Wiki/Mod-Creation/Developer-Reference/Sound-%E2%80%90-Wwise-Events/")]
+        public static void akplaysound(ConCommandArgs args)
+        {
+            Debug.Log("args = " + args[0]);
+            Debug.Log($"sender body = {args.senderBody}");
+            uint soundid = AkSoundEngine.PostEvent(args[0], ((Component)(object)args.senderBody).gameObject);
+            Debug.Log($"sound id = {soundid}");
+            sounds.Add(soundid);
+        }
+
+        [ConCommand(commandName = "stop_sound", flags = ConVarFlags.None, helpText = "stop looping sounds from playsound !!!!!")]
+        public static void akstopsound(ConCommandArgs args)
+        {
+            Debug.Log($"sender body = {args.senderBody}");
+            foreach (uint sound in sounds.ToList())
+            {
+                AkSoundEngine.StopPlayingID(sound);
+                Debug.Log($"stopped sound {sound} !!!");
+                sounds.Remove(sound);
+            }
+        }
+        
+        [ConCommand(commandName = "play_effect", flags = ConVarFlags.None, helpText = "play an effect !!!!!")]
+        [ConCommand(commandName = "spawn_effect", flags = ConVarFlags.None, helpText = "play an effect !!!!!")]
+        public static void spawnEffect(ConCommandArgs args)
+        {
+            Debug.Log("args = " + args[0] + " " + args[1]);
+            
+            EffectDef effect = null;
+            foreach (EffectDef effectDef in EffectCatalog.entries)
+            {
+                if (!effectDef.prefabName.Contains(args[0])) continue;
+                
+                effect = effectDef;
+                break;
+            }
+
+            if (effect == null)
+            {
+                Log.Warning($"couldnt find effect {args[0]} !!!");
+                return;
+            }
+            
+            EffectManager.SpawnEffect(effect.index, new EffectData
+            {
+                origin = args.senderBody.transform.position,
+                scale = Convert.ToSingle(args[1]),
+                rotation = Util.QuaternionSafeLookRotation(Vector3.up)
+            }, true);
         }
     }
 }
